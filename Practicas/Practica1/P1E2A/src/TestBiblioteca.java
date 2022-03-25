c83e9533b69a7de84e077267372e8a2524331cc7
OmgCopito95 <july1995bolt@gmail.com>
2022-03-24 23:14:40 -0300

    Ejercicio 2a procesando el string json sin libreria

---
+++ Practicas/Practica1/P1E2A/src/TestBiblioteca.java
@@ -0,0 +1,62 @@
+import static org.junit.Assert.assertEquals;
+
+import org.junit.Before;
+import org.junit.Test;
+
+// test biblioteca
+
+public class TestBiblioteca {
+
+    private Biblioteca biblioteca;
+    private Socio s1, s2;
+
+    @Before
+    public void setUp() {
+        biblioteca = new Biblioteca();
+        s1 = new Socio("Arya Stark", "test@test.com", "5234-5");
+        s2 = new Socio("Tyron Lannister", "tyron@thelannisters.com", "2345-2");
+        biblioteca.agregarSocio(s1);
+        biblioteca.agregarSocio(s2);
+    }
+
+    @Test
+    public void test() {
+
+        // exporta socios
+        String json = biblioteca.exportarSocios();
+
+        // elimina llaves corchetes
+        json = json.replaceAll("\\[", "");
+        json = json.replaceAll("\\{", "");
+        json = json.replaceAll("\\]", "");
+        json = json.replaceAll("\\}", "");
+
+        // elimina los espacios antes de una comilla
+        json = json.replaceAll("\"\\s+", "\"");
+        // elimina los espacios despues de una comilla
+        json = json.replaceAll("\\s+\"", "\"");
+
+        // arma un array con los datos del socio
+        String[] datos = json.split(",");
+
+        //por cada elemento del array se queda con el segundo texto entre comillas
+        for (int i = 0; i < datos.length; i++) {
+            datos[i] = datos[i].replaceAll("\"", "").split(":")[1];
+        }
+
+        // System.out.println(datos[0]);
+
+        //socio 1
+        assertEquals(datos[0], s1.getNombre());
+        assertEquals(datos[1], s1.getEmail());
+        assertEquals(datos[2], s1.getLegajo());
+
+        //socio 2
+        assertEquals(datos[3], s2.getNombre());
+        assertEquals(datos[4], s2.getEmail());
+        assertEquals(datos[5], s2.getLegajo());
+
+
+    }
+
+}
