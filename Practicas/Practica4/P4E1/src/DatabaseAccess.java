d195ec343a80802980af3b68a238d7be34c3e717
OmgCopito95 <july1995bolt@gmail.com>
2022-04-17 19:06:30 -0300

    ejercicio 4 con proxy

---
+++ Practicas/Practica4/P4E1/src/DatabaseAccess.java
@@ -0,0 +1,8 @@
+import java.util.Collection;
+import java.util.List;
+
+public interface DatabaseAccess {
+    Collection<String> getSearchResults(String queryString);
+
+    int insertNewRow(List<String> rowData);
+}
