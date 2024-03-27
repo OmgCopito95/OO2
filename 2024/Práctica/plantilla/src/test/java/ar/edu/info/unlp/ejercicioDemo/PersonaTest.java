db82c8d86e9939affd1526ee3080c522509f583e
Julieta <julietalanciotti@gmail.com>
2024-03-27 12:05:09 -0300

    cambios

--- 2024/Práctica/plantilla/src/test/java/ar/edu/info/unlp/ejercicioDemo/PersonaTest.java
+++ 2024/Práctica/plantilla/src/test/java/ar/edu/info/unlp/ejercicioDemo/PersonaTest.java
@@ -1,4 +1,4 @@
-package ar.edu.info.unlp.ejercicio;
+package ar.edu.info.unlp.ejercicioDemo;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import org.junit.jupiter.api.BeforeEach;
